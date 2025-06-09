d9ae099ba457fd5a8f0698af74d6f200fd4ab51d
Ignacio <ignacionazar9@gmail.com>
2025-06-09 00:21:00 -0300

    agregue un mensaje cuando edito una pelicula y cuando borro una pelicula

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ import Pelicula from "./classPelicula.js";
 
 //funciones
 const abrirModal = () => {
-  const modalPelicula = new bootstrap.Modal(
-    document.getElementById("modalPelicula")
-  );
+  //aqui abro la ventana modal
   modalPelicula.show();
+  //cambie la variable para que cree peliculas
+  creandoPelicula = true;
 };
 
 const crearPelicula = () => {
@@ -29,11 +29,10 @@ const crearPelicula = () => {
   limpiarFormulario();
   //mostrar un mensaje al usuario indicando que se creo la pelicula
   Swal.fire({
-  title: "Pelicula creada",
-  text: `La pelicula ${peliculaNueva.nombre}, fue creada correctamente`,
-  icon: "success",
-});
-
+    title: "Pelicula creada",
+    text: `La pelicula ${peliculaNueva.nombre}, fue creada correctamente`,
+    icon: "success",
+  });
 };
 
 const limpiarFormulario = () => {
@@ -70,57 +69,100 @@ const dibujarFila = (pelicula, indice) => {
               </td>
             </tr>`;
 };
-window.borrarPelicula = (id)=>{
-  console.log("aqui deberia borrar una pelicula")
-  console.log(id)
-  //aqui voy a mostrar un mensaje de confirmacion al borrar una pelicula
-  /*Swal.fire({
-  title: "Are you sure?",
-  text: "You won't be able to revert this!",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#3085d6",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Yes, delete it!"
-}).then((result) => {
-  if (result.isConfirmed) {
-    Swal.fire({
-      title: "Deleted!",
-      text: "Your file has been deleted.",
-      icon: "success"
-    });
-  }
-});*/
-  //buscar y borrar la pelicula del array cartelera
-  const posicionPeliculaBuscada = cartelera.findIndex((pelicula)=> pelicula.id === id)
-  cartelera.splice(posicionPeliculaBuscada, 1)
-  //actualizar el localstorage
-  guardarLocalStorage()
-  //actualizar la tabla de peliculas
-  console.log(posicionPeliculaBuscada)
-  tablaPeliculas.children[posicionPeliculaBuscada].remove()
-  //todo: corregir las celdas de la tabla cuando borramos una pelicula
-}
-
-window.prepararPelicula = (id)=>{
-  console.log("aqui tengo que preparar la pelicula", id)
+window.borrarPelicula = (id) => {
+  Swal.fire({
+    title: "Estas por eliminar una pelicula",
+    text: "si decides eliminar, no puedes revertir este paso",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    cancelButtonText: "Salir",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      //buscar y borrar la pelicula del array cartelera
+      const posicionPeliculaBuscada = cartelera.findIndex(
+        (pelicula) => pelicula.id === id
+      );
+      cartelera.splice(posicionPeliculaBuscada, 1);
+      //actualizar el localstorage
+      guardarLocalStorage();
+      //actualizar la tabla de peliculas
+      console.log(posicionPeliculaBuscada);
+      tablaPeliculas.children[posicionPeliculaBuscada].remove();
+      //todo: corregir las celdas de la tabla cuando borramos una pelicula
+      //Recorrer las filas restantes y actualizar sus indices
+      const filasRestantes = tablaPeliculas.children;
+      for (let i = 0; i < filasRestantes.length; i++) {
+        const celdaIndice = filasRestantes[i].querySelector("td");
+        if (celdaIndice) {
+          celdaIndice.textContent = i + 1; //actualiza el texto con el nuevo indice
+        }
+      }
+    }
+  });
+};
+window.prepararPelicula = (id) => {
+  console.log("aqui tengo que preparar la pelicula", id);
   //abrir el modal
-  abrirModal()
+  abrirModal();
   //cargar los datos en el modal
-  const peliculaBuscada = cartelera.find((pelicula)=> pelicula.id === id)
-  inputNombre.value = peliculaBuscada.nombre
-  inputGenero.value = peliculaBuscada.genero
-  inputDirector.value = peliculaBuscada.director
-  inputDuracion.value = peliculaBuscada.duracion
-  inputImagen.value = peliculaBuscada.imagen
-  inputDescripcion.value = peliculaBuscada.descripcion
-  
-  //abrir el modal
-  abrirModal()
-}
+  const peliculaBuscada = cartelera.find((pelicula) => pelicula.id === id);
+  inputNombre.value = peliculaBuscada.nombre;
+  inputGenero.value = peliculaBuscada.genero;
+  inputDirector.value = peliculaBuscada.director;
+  inputDuracion.value = peliculaBuscada.duracion;
+  inputImagen.value = peliculaBuscada.imagen;
+  inputDescripcion.value = peliculaBuscada.descripcion;
 
+  //abrir el modal
+  abrirModal();
+  //guardo el id del contacto que quiero editar
+  idPeliculaEditar = id;
+  creandoPelicula = false;
+};
+
+const editarPelicula = () => {
+  console.log("aqui debo agregar la logica que edite la pelicula en el array");
+  //agarrar los datos del formulario y actualizarlos dentro del array cartelera
+  const posicionPelicula = cartelera.findIndex(
+    (pelicula) => pelicula.id === idPeliculaEditar
+  );
+  console.log(posicionPelicula);
+  cartelera[posicionPelicula].nombre = inputNombre.value;
+  cartelera[posicionPelicula].genero = inputGenero.value;
+  cartelera[posicionPelicula].director = inputDirector.value;
+  cartelera[posicionPelicula].duracion = inputDuracion.value;
+  cartelera[posicionPelicula].imagen = inputImagen.value;
+  cartelera[posicionPelicula].descripcion = inputDescripcion.value;
+  //actualizar el localstorage
+  guardarLocalStorage();
+  //limpiar el formulario
+  limpiarFormulario();
+  //cerrar el modal
+  modalPelicula.hide();
+  //actualizar la tabla de peliculas
+  //actualizar SOLO la fila de la tabla correspondiente a la pelicula editada
+  const filaEditada = tablaPeliculas.children[posicionPelicula];
+  if (filaEditada) {
+    filaEditada.children[1].textContent = cartelera[posicionPelicula].nombre;
+    filaEditada.children[2].textContent = cartelera[posicionPelicula].genero;
+    filaEditada.children[3].textContent = cartelera[posicionPelicula].director;
+    filaEditada.children[4].textContent = cartelera[posicionPelicula].duracion;
+  }
+  //agregar un mensaje al usuario
+  Swal.fire({
+    title: "Pelicula modificada",
+    text: `La pelicula ${cartelera[posicionPelicula].nombre}, fue modificada correctamente`,
+    icon: "success",
+  });
+};
 
 //declarar variables
+const modalPelicula = new bootstrap.Modal(
+  document.getElementById("modalPelicula")
+);
 const btnAgregar = document.getElementById("btnAgregar");
 const formularioPelicula = document.querySelector("form");
 const inputNombre = document.querySelector("#nombre");
@@ -131,13 +173,19 @@ const inputImagen = document.querySelector("#imagen");
 const inputDescripcion = document.querySelector("#descripción");
 const cartelera = JSON.parse(localStorage.getItem("carteleraKey")) || [];
 const tablaPeliculas = document.querySelector("tbody");
+let idPeliculaEditar = null;
+let creandoPelicula = true;
 
 //agrego los manejadores de eventos
 btnAgregar.addEventListener("click", abrirModal);
 formularioPelicula.addEventListener("submit", (e) => {
   e.preventDefault();
-  //aqui voy a crear un pelicula
-  crearPelicula();
+  if (creandoPelicula) {
+    //aqui voy a crear un pelicula
+    crearPelicula();
+  } else {
+    editarPelicula();
+  }
   //algun dia aqui voy a editar un pelicula
 });
 
